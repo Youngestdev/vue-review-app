d9653cc684ae15d7fbd7f7403a79faa337229a7d
Abdulazeez Abdulazeez Adeshina <31009679+Youngestdev@users.noreply.github.com>
2018-09-16 00:47:51 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Reviews>
+    <reviewitem></reviewitem>
+    </Reviews>
+  </div>
+</template>
+
+<script>
+import Reviews from "./components/Reviews"
+import reviewitem from "./components/Review-Item"
+
+export default {
+  name: "App",
+  components: {
+    Reviews,
+    reviewitem
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Review-Item.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+      {{comment}}
+      <span><StarRating v-model=rating :read-only=true :star-size=20 :show-rating=false></StarRating></span>
+  </li>
+</template>
+
+<script>
+import StarRating from 'vue-star-rating'
+import Reviews from './Reviews'
+export default {
+  name: "reviewitem",
+  props: ['comment', 'rating'],
+  components: {
+      StarRating
+  }
+}
+</script>
